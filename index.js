a30df7455a9c8c0e4bd972f31b725007be889ab3
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-11-10 09:08:01 -0300

    Install axios and get dara from firebase

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const content = await axios.get('https://como-fazer-9e94a.firebaseio.com/teste.json')
+  res.render('index', { content: content.data })
+})
+
+app.get('/categorias/nova', (req, res) => {
+  res.render('categorias/nova')
+})
+
+app.post('/categorias/nova', (req, res) => {
+  console.log(res)
 })
 
 app.listen(port, err => {
