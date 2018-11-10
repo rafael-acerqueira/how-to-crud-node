ec7352e665bbc04ed8e0ca3e2c49f40435a74056
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-11-10 09:22:44 -0300

    Add bodyParser and add category into firebase

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
-
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded())
 
 app.get('/', async (req, res) => {
   const content = await axios.get('https://como-fazer-9e94a.firebaseio.com/teste.json')
@@ -15,8 +16,9 @@ app.get('/categorias/nova', (req, res) => {
   res.render('categorias/nova')
 })
 
-app.post('/categorias/nova', (req, res) => {
-  console.log(res)
+app.post('/categorias/nova', async (req, res) => {
+  await axios.post('https://como-fazer-9e94a.firebaseio.com/categorias.json', req.body)
+  res.render('categorias/nova')
 })
 
 app.listen(port, err => {
