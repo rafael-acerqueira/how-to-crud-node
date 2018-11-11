47939ff55ef04b61af642120a8d9cb131d870809
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-11-11 10:50:26 -0300

    Add CRUD publication with relationship with categories

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+
 const categorias = require('./routes/categorias')
+const publicacoes = require('./routes/publicacoes')
+
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded())
+
 app.use('/categorias', categorias)
+app.use('/publicacoes', publicacoes)
 
 app.get('/', async (req, res) => {
   res.render('index')
