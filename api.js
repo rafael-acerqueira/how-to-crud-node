dcbd435523996e51ac865c8678d2120eb0174d2b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-11-11 18:30:09 -0300

    Add auth to firebase

--- api.js
+++ api.js
@@ -1,9 +1,10 @@
 const axios = require('axios')
 
 const baseURL = 'https://como-fazer-9e94a.firebaseio.com/'
+const auth = 'f47RCtCfvJY6NlZ2y3WLdj7fgELVCcyvPUS8fxO9'
 
 const list = async (key) => {
-  const content = await axios.get(baseURL + key + '.json')
+  const content = await axios.get(baseURL + key + '.json?auth=' + auth)
   if(content.data){
     const objetos = Object
                           .keys(content.data)
@@ -19,12 +20,12 @@ const list = async (key) => {
 }
 
 const apagar = async (key, id) => {
-  await axios.delete(`${baseURL}/${key}/${id}.json`)
+  await axios.delete(`${baseURL}/${key}/${id}.json?auth=` + auth)
   return true
 }
 
 const get = async (key, id) => {
-  const content = await axios.get(`${baseURL}/${key}/${id}.json`)
+  const content = await axios.get(`${baseURL}/${key}/${id}.json?auth=` + auth)
   return {
      id,
      ...content.data
@@ -32,12 +33,12 @@ const get = async (key, id) => {
 }
 
 const update = async (key, id, data) => {
-  await axios.put(`${baseURL}/${key}/${id}.json`, data)
+  await axios.put(`${baseURL}/${key}/${id}.json?auth=` + auth, data)
   return true
 }
 
 const create = async(key, data) => {
-  await axios.post(`${baseURL}/${key}.json`, data)
+  await axios.post(`${baseURL}/${key}.json?auth=` + auth, data)
   return true
 }
 
